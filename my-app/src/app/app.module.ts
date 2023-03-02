63fd009b7022a627bdea17d9a698b8f1c3f122c8
dovakin0007 <73059450+dovakin0007@users.noreply.github.com>
2023-03-02 18:31:03 +0530

    6 files modified, 20 added

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,8 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Hello } from './hello.component';
 
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+
 @NgModule({
-  declarations: [AppComponent, Hello],
+  declarations: [
+    AppComponent,
+    Hello,
+    NavBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainContentComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
