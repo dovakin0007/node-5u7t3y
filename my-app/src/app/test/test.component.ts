0500dbfea8b72555ff405c076e95094f2f4f28b4
dovakin0007 <73059450+dovakin0007@users.noreply.github.com>
2023-03-03 18:22:12 +0530

    2 files modified

--- my-app/src/app/test/test.component.ts
+++ my-app/src/app/test/test.component.ts
@@ -2,14 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: `
-  <h1>{{name}}</h1>
-  `,
+  template: `
+  <h1>{{title}}</h1>
+  
+  <p>Heroes:</p>
+  <ul>
+    <li *ngFor="let hero of heroes">
+      {{ hero }}
+    </li>
+  </ul>
+  <button [disabled]="isDisabled"> with interpolation </button>
+  <button disabled={{isDisabled}}> with property </button>
+`,
+
   styleUrls: ['./test.component.css'],
 })
-export class TestComponent implements OnInit {
-  name = 'Gowtham';
-
-  constructor() {}
-  ngOnInit() {}
+export class TestComponent {
+  title = 'Tour of Heroes';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  myHero = this.heroes[0];
+  isDisabled = false;
 }
